f82f42852f71357ef0f82a66bf9b2ec900516e20
bpoole53 <bpoole53@gmail.com>
2023-07-28 11:50:31 -0500

    added the inquirer code and the write file, need to adjust it this project

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -11,3 +13,36 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What is your location?',
+      name: 'location',
+    },
+    {
+      type: 'input',
+      message: 'Please provide a link to your bio.',
+      name: 'bio',
+    },
+    {
+      type: 'input',
+      message: 'Please provide your linkedin URL.',
+      name: 'linkedin',
+    },
+    {
+      type: 'input',
+      message: 'Please provide your github URL.',
+      name: 'github',
+    },
+  ])
+  .then((data) => {
+    fs.writeFile('README.md', /*put the data here*/'test', (err) =>
+        err ? console.log(err) : console.log('Successfully created html file!')        
+    );
+  });
